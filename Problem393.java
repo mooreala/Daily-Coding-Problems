56231d685ba243c000eaf188ee68756353ef4885
Alana Moore <polkafrogg@gmail.com>
2021-11-10 14:51:43 -0800

    Setup class for Problem # 393 :

---
+++ Problem393.java
@@ -0,0 +1,15 @@
+/**
+ * Problem: Given an array of integers, return the largest range, inclusive, of 
+ * integers that are all included in the array.
+ */
+
+public class Problem393 {
+
+    public int[] largestRange(int[] arr) {
+
+    }
+    public static void main(String args[]) {
+        int[] arr = {9, 6, 1, 3, 8, 10, 12, 11};
+        int[] sol = largestRange(arr);
+    }
+}
